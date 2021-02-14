a6a0196959b8c9a8c1d6ef60ad83e908cacfa255
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2021-02-14 13:24:38 +0100

    Possible fix;

--- hooks/abortFetch.ts
+++ hooks/abortFetch.ts
@@ -2,7 +2,7 @@ import { useState, useEffect, useMemo } from "react";
 import dayjs from "dayjs";
 
 export function useAbortFetch<T>(fetcher: (controller: AbortController, ...props: any) => Promise<T>, ...props: any): [T | null, () => Promise<void>] {
-    const abortController = new AbortController();
+    const abortController = new window.AbortController();
     const [resource, setResource] = useState<T | null | undefined>(undefined);
     const [lastFetch, setLastFetch] = useState(dayjs().unix());
 
