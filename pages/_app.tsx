f05ca8138a5775d79ef8cf08e1c8ee967dd37371
Michael Ketzer <michael.ketzer@hcom.de>
2021-02-15 09:14:36 +0100

    Adjusted sentry reporting;

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,9 @@
-import * as Sentry from '@sentry/browser';
+import * as Sentry from '@sentry/react';
 import App, { AppInitialProps } from 'next/app';
-import { Integrations } from '@sentry/apm';
 import Head from "next/head";
 
 if (process.env.SENTRY_DSN && process.env.SENTRY_DSN.length > 0) {
-    Sentry.init({
-        dsn: process.env.SENTRY_DSN,
-        integrations: [
-            new Integrations.Tracing(),
-        ],
-        tracesSampleRate: 0.5,
-    });
+    Sentry.init({dsn: process.env.SENTRY_DSN});
 }
 
 class MyApp extends App<AppInitialProps> {
