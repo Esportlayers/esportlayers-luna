b12246a750eed149843cbd7ce1850c13cbbcb55e
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2021-02-16 18:34:30 +0100

    Test

--- pages/testpage.tsx
+++ pages/testpage.tsx
@@ -2,6 +2,8 @@ import { ReactElement } from "react";
 
 
 export default function Testpage(): ReactElement {
-    const a = () => new Error('test');
+    const a = () => {
+        throw new Error('test');
+    }
     return <button onClick={a}>Error</button>;
 }
