e166b7a2a873f30e14541fcabf4bb6de0d7fa4ce
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2021-03-14 09:17:21 +0100

    Adjusted anti snipe overlays;

--- components/pages/antiSnipe.tsx
+++ components/pages/antiSnipe.tsx
@@ -1,31 +1,41 @@
-import { ReactElement } from "react";
-import getWebsocketUrl from "../../modules/Router";
-import Tether from "@esportlayers/io";
-import { useAbortFetch } from "../../hooks/abortFetch";
-import { fetchUser } from "../dotaStats/Overlay";
 import Overlay from "../antiSnipe/Overlay";
+import { ReactElement } from "react";
+import Tether from "@esportlayers/io";
+import { fetchUser } from "../dotaStats/Overlay";
+import getWebsocketUrl from "../../modules/Router";
+import { useAbortFetch } from "../../hooks/abortFetch";
 
-export default function AntiSnipePage({auth, testing}: {auth: string, testing: boolean}): ReactElement {
-    const [user] = useAbortFetch(fetchUser, auth);
-    if(user && Boolean(user.useMinimapOverlay)) {
-        return <Tether url={getWebsocketUrl()+'/dota-gsi/live/' + auth}>
-            <div className={'antiSnipe'}>
-                <Overlay frameKey={auth} testing={testing}/>
-            </div>
+export default function AntiSnipePage({
+  auth,
+  testing,
+}: {
+  auth: string;
+  testing: boolean;
+}): ReactElement {
+  const [user] = useAbortFetch(fetchUser, auth);
+  if (user && Boolean(user.useMinimapOverlay)) {
+    return (
+      <Tether url={getWebsocketUrl() + "/dota-gsi/live/" + auth}>
+        <div className={"antiSnipe"}>
+          <Overlay frameKey={auth} testing={testing} />
+        </div>
 
-            <style global jsx>{`
-                html, body {
-                    padding-bottom: 3px;
-                    background-color: transparent;
-                    margin: 0;
-                }
+        <style global jsx>{`
+          html,
+          body {
+            padding-bottom: 3px;
+            background-color: transparent;
+            margin: 0;
+          }
 
-                .antiSnipe {
-                    width: 283px;
-                }
-            `}</style>
-        </Tether>;
-    }
+          .antiSnipe {
+            width: 243px;
+            height: 243px;
+          }
+        `}</style>
+      </Tether>
+    );
+  }
 
-    return null;
+  return null;
 }
