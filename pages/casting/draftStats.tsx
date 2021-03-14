653a0f5aa652e9faecb17d3987e0dcd8afe47c3a
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2021-03-14 19:10:18 +0100

    Disabled draft stats;

--- pages/casting/draftStats.tsx
+++ pages/casting/draftStats.tsx
@@ -1,17 +1,12 @@
 import { ReactElement } from "react";
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
 
-const DraftStatsPage = dynamic(
-    () => import('../../components/pages/casting/draftStats'),
-    { ssr: false }
-);
-
-function DraftStats(props): ReactElement {
-    return <DraftStatsPage {...props} />
+function DraftStats(): ReactElement {
+  return <h1>404 - Page Not Found</h1>;
 }
 
-DraftStats.getInitialProps = ({query: {auth, testing}}) => {
-    return {auth, testing: Boolean(testing)};
-}
+DraftStats.getInitialProps = ({ query: { auth, testing } }) => {
+  return { auth, testing: Boolean(testing) };
+};
 
 export default DraftStats;
